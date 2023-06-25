b4ca6528acfa783ce93c3c66f7b2607268552669
vaishnavihole <vaishnavihole1@gmail.com>
2023-06-25 09:47:52 +0530

    dates with arguments

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,8 @@
-// date-string method
-const d = new Date('2023-06-23');
-console.log(d);
+// *******date-string method******
+// const d = new Date('2023-06-23');
+// console.log(d);
+
+// ******** Creating dates with arguments *******
+  const d = new Date(2023, 6, 30, 7, 30, 59)
+  console.log(d);
+
