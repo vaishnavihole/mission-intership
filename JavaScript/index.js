e4d5dd5cc25d78e72752976c0ede7716d12b42dc
vaishnavihole <vaishnavihole1@gmail.com>
2023-06-25 09:52:52 +0530

    use Writing a custom date format

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -3,6 +3,16 @@
 // console.log(d);
 
 // ******** Creating dates with arguments *******
-  const d = new Date(2023, 6, 30, 7, 30, 59)
-  console.log(d);
+//   const d = new Date(2023, 6, 30, 7, 30, 59)
+//   console.log(d);
 
+
+// ***** With no arguments
+// const d = new Date('2023-06-23')
+// console.log(d);
+
+// Writing a custom date format
+const d = new Date('2023-06-23')
+const year = d.getFullYear()
+const date = d.getDate()
+console.log(date,year);
